3b9633f1c2d7f83e7b950afc5bc957c7fae3583a
souravmalik8499 <souravmalik8499@gmail.com>
2025-05-30 01:47:13 +0530

    Using weather API

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -6,15 +6,36 @@ import "./SearchBox.css";
 
 export default function SearchBox(){
     let [city, setCity] = useState("");
+    // State to hold the city name input by the user
+    const API_URL = "https://api.openweathermap.org/data/2.5/weather";
+    const API_KEY = "0188c9338757c06fb198148ea2b45c8d";
+
+    let getWeatherInfo = async () => {
+       let response = await fetch(`${API_URL}?q=${city}&appid=${API_KEY}&units=metric`);
+       let jsonResponse = await response.json();
+       console.log(jsonResponse);
+       let result = {
+        temp: jsonResponse.main.temp,
+        tempMin: jsonResponse.main.temp_min,
+        tempMax: jsonResponse.main.temp_max,
+        humidity: jsonResponse.main.humidity,
+        feelsLike: jsonResponse.main.feels_like,
+        Weather: jsonResponse.weather[0].description,
+       };
+       console.log(result);
+        
+    };
+
+    
     let handleChange = (evt) => {
         setCity(evt.target.value);
     };
 
     let handleSubmit = (evt) => {
         evt.preventDefault();
-        console.log("City name submitted: ", city);
+        console.log(city);
         setCity("");
-        
+        getWeatherInfo();
     }
 
     return (
