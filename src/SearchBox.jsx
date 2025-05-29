0e0e8c65a4bf13995c83db73c15aacbc3ee25571
souravmalik8499 <souravmalik8499@gmail.com>
2025-05-29 20:18:57 +0530

    Building Search Box

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -1,12 +1,29 @@
+import React, { useState } from 'react';
+
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import "./SearchBox.css";
 
 export default function SearchBox(){
+    let [city, setCity] = useState("");
+    let handleChange = (evt) => {
+        setCity(evt.target.value);
+    };
+
+    let handleSubmit = (evt) => {
+        evt.preventDefault();
+        console.log("City name submitted: ", city);
+        setCity("");
+        
+    }
+
     return (
-        <div className='search-box'>
+        <div className='Search-box'>
             <h3>Search for the Weather</h3>
-            <form action="">
-                <TextField id="city" label="City Name" variant="outlined" required />
+            <form action="" onSubmit={handleSubmit}>
+                <TextField id="city" label="City Name" 
+                variant="outlined" required value={city}
+                 onChange={handleChange}/>
                 <br /><br />
             <Button variant="contained" type="submit" >
             Search
